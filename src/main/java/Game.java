f5e19b5eba732b931e59d9b0fb3d84b34494cb1f
xayblln <askarxay@yandex.ru>
2023-05-09 18:22:06 +0300

    Add Window

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,47 @@
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable {
+    private boolean running;
+    public static int WIDTH = 700;
+    public static int HEIGHT = 500;
+    public static String NAME = "GAME";
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Game game = new Game();
+        game.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        JFrame frame = new JFrame(Game.NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(game);
+        frame.pack();
+        frame.setVisible(true);
+        game.start();
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            render();
+        }
+    }
+
+    public void start() {
+        running = true;
+        new Thread(this).start();
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(2);
+            requestFocus();
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.black);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.dispose();
+        bs.show();
     }
 }
